d5143feab7a775c10aa708d5936c0a714adebef9
Hugo Campos <hgcampos96@outlook.com>
2023-08-09 13:15:19 -0300

    Desafios de Código Kotlin na DIO

--- 7_praticando_com_desafios_de_codigo_kotlin_na_dio/3_taxa_de_crescimento/exemplo1.kt
+++ 7_praticando_com_desafios_de_codigo_kotlin_na_dio/3_taxa_de_crescimento/exemplo1.kt
@@ -1,19 +1,20 @@
 data class Pais(val habitantes: Double, val taxaCrescimento: Double) {
-    fun crescerPopulacaoAnual() { 
-      habitantes += habitantes * (taxaCrescimento / 100)
+    fun crescerPopulacaoAnual(): Pais { 
+      val novoHabitantes = habitantes + habitantes * (taxaCrescimento / 100)
+      return Pais(novoHabitantes, taxaCrescimento)
     }
 }
 
 fun main() {
-   val habitantesPaisA = readLine()!!.toDouble();
-   val habitantesPaisB = readLine()!!.toDouble();
-   val paisA = Pais(habitantesPaisA, taxaCrescimento = 3.0)
-   val paisB = Pais(habitantesPaisB, taxaCrescimento = 1.5)
+   val habitantesPaisA = readLine()!!.toDouble()
+   val habitantesPaisB = readLine()!!.toDouble()
+   var paisA = Pais(habitantesPaisA, taxaCrescimento = 3.0)
+   var paisB = Pais(habitantesPaisB, taxaCrescimento = 1.5)
    
    var quantidadeAnos = 0
    while (paisA.habitantes < paisB.habitantes) {
-    paisA.crescerPopulacaoAnual()
-    paisB.crescerPopulacaoAnual()
+    paisA = paisA.crescerPopulacaoAnual()
+    paisB = paisB.crescerPopulacaoAnual()
     quantidadeAnos++
    }
    
